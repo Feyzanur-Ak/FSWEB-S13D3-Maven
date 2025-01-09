7b004d4ab50b14829f2d058650e8321571c00a54
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-09 12:57:09 +0300

    Constructor erişim düzenlenmesi

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -10,14 +10,14 @@ public class Person {
     String city;
 
 
-     Person(String firstName,  String lastName, int age){
+     public Person(String firstName,  String lastName, int age){
       this.firstName=firstName;
       this.lastName=lastName;
       this.age=age;
 
     }
 
-    Person(String firstName,  String lastName, int age, String job,  double salary, String city){
+    public Person(String firstName,  String lastName, int age, String job,  double salary, String city){
          this(firstName, lastName, age);
         this.job=job;
         this.salary=salary;
