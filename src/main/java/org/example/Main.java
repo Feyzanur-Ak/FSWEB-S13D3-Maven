2c01280ce438ecb81cc60cc7fe499a96695195ac
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-09 12:55:39 +0300

    isTeen ismi düzenlendi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        Person person1=new Person("John", "Doe", 14,true);
+        Person person1=new Person("John", "Doe", 14);
         System.out.println("Firstname: " + person1.getFirstName());
         System.out.println("LastName: " + person1.getLastName());
         System.out.println("Age: " + person1.getAge());
-        System.out.println("Is Teen: " + person1.getTeen());
+        System.out.println("Is Teen: " + person1.isTeen());
 
     }
 
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -8,17 +8,17 @@ public class Person {
     String job;
     double salary;
     String city;
-    boolean isTeen;
 
-     Person(String firstName,  String lastName, int age, boolean isTeen){
+
+     Person(String firstName,  String lastName, int age){
       this.firstName=firstName;
       this.lastName=lastName;
       this.age=age;
-      this.isTeen=isTeen;
+
     }
 
-    Person(String firstName,  String lastName, int age, boolean isTeen, String job,  double salary, String city){
-         this(firstName, lastName, age,isTeen);
+    Person(String firstName,  String lastName, int age, String job,  double salary, String city){
+         this(firstName, lastName, age);
         this.job=job;
         this.salary=salary;
         this.city=city;
@@ -36,7 +36,7 @@ public class Person {
         return  age;
     }
 
-    public boolean getTeen(){
+    public boolean isTeen(){
         return age >= 13 && age <= 19;
     }
 }
