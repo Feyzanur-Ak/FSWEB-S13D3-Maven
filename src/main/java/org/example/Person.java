d5c84b76575e9953054b78ff1de23decf1492b82
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-09 12:13:44 +0300

    Constructor yapıları oluşturuldu

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class Person {
+
+    String firstName;
+    String lastName;
+    int age;
+    String job;
+    double salary;
+    String city;
+
+     Person(String firstName,  String lastName, int age){
+      this.firstName=firstName;
+      this.lastName=lastName;
+      this.age=age;
+    }
+
+    Person(String firstName,  String lastName, int age, String job,  double salary, String city){
+         this(firstName, lastName, age);
+        this.job=job;
+        this.salary=salary;
+        this.city=city;
+    }
 }
